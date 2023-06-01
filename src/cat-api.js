85e2bb512b564ad7ccf5d4460e76ed9e9e29a53a
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-06-01 22:03:26 +0300

    1

---
+++ src/cat-api.js
@@ -0,0 +1,39 @@
+const apiKey = 'live_9i6HzzZFNFA1oaYRze81AbtKlBywv3OJ6502fJjo0pAUtFOdj61V5PlPH5ga6JCl';
+// Функція для виконання запиту на отримання списку порід
+export function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds', {
+      headers: {
+        'x-api-key': apiKey,
+      },
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch breeds');
+      }
+      return response.json();
+    });
+  }
+  // Функція для виконання запиту на отримання інформації про кота за породою
+  export function fetchCatByBreed(breedId) {
+    const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+    return fetch(url, {
+      headers: {
+        'x-api-key': apiKey,
+      },
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch cat by breed');
+      }
+      return response.json().then((data) => {
+        if (data.length > 0) {
+          return {
+            name: data[0].breeds[0].name,
+            description: data[0].breeds[0].description,
+            temperament: data[0].breeds[0].temperament,
+            url: data[0].url,
+          };
+        } else {
+          throw new Error('No cat found for the selected breed');
+        }
+      });
+    });
+  }
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+// Функція для заповнення селекту порід
+function populateBreeds(breeds) {
+  breeds.forEach((breed) => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  });
+}
+// Функція для відображення інформації про кота
+function displayCatInfo(cat) {
+  catInfo.innerHTML = `
+    <img src="${cat.url}" alt="Cat Image">
+    <p>Name: ${cat.name}</p>
+    <p>Description: ${cat.description}</p>
+    <p>Temperament: ${cat.temperament}</p>
+  `;
+}
+// Обробник події при зміні вибраної опції в селекті
+breedSelect.addEventListener('change', (event) => {
+  const selectedBreedId = event.target.value;
+  if (selectedBreedId) {
+    loader.style.display = 'block';
+    catInfo.style.display = 'none';
+    error.style.display = 'none';
+    fetchCatByBreed(selectedBreedId)
+      .then((cat) => {
+        displayCatInfo(cat);
+        loader.style.display = 'none';
+        catInfo.style.display = 'block';
+      })
+      .catch(() => {
+        error.style.display = 'block';
+        loader.style.display = 'none';
+      });
+  }
+});
+// Виклик функції для отримання та заповнення списку порід
+loader.style.display = 'block';
+fetchBreeds()
+  .then((breeds) => {
+    populateBreeds(breeds);
+    loader.style.display = 'none';
+    breedSelect.style.display = 'block';
+  })
+  .catch(() => {
+    error.style.display = 'block';
+    loader.style.display = 'none';
+  });
